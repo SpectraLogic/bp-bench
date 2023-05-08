1058d5e64c4ff79c605eff1a0bae18be4f3b044f
Sean Arms <67096+lesserwhirls@users.noreply.github.com>
2023-05-08 12:19:49 -0600

    ESCP-3906: Gradle Improvements (#14)
    * Upgrade to gradle 8.1.1
    
    * Manage dependencies in settings.gradle.kts
    
    * Allow gradle to manage jdk toolchain
    
    * Keep jitpack until ds3 sdk published to maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,43 +1,53 @@
 /*
  * ****************************************************************************
- *   Copyright 2014-2019 Spectra Logic Corporation. All Rights Reserved.
+ *   Copyright 2014-2023 Spectra Logic Corporation. All Rights Reserved.
  * ***************************************************************************
  */
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.10"
+    alias(libs.plugins.kotlinJvm)
     application
+    alias(libs.plugins.owaspDepCheck)
+    alias(libs.plugins.versions)
 }
 
 group = "com.spectralogic"
-tasks {
-    withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "18"
+
+dependencies {
+    implementation(platform(libs.kotlinBom))
+
+    implementation(libs.cliktJvm)
+    implementation(libs.ds3sdk)
+    implementation(libs.kotlinReflect)
+    implementation(libs.kotlinStdlib)
+    implementation(libs.kotlinxCoroutinesCore)
+
+    testImplementation(libs.kotlinTestRunnerJunit5)
+    testImplementation(libs.mockk)
+}
+
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(18))
+        vendor.set(JvmVendorSpec.ADOPTIUM)
     }
 }
 
-repositories {
-    mavenCentral()
-    maven("https://jitpack.io")
-}
-
-val test by tasks.getting(Test::class) {
+tasks.test {
     useJUnitPlatform { }
 }
 
-dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.7.10")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.10")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("com.github.ajalt.clikt:clikt-jvm:3.5.0")
-
-    implementation("com.github.spectralogic.ds3_java_sdk:ds3-sdk:5.4.0")
-
-    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
-    testImplementation("io.mockk:mockk:1.12.4")
+tasks.compileKotlin {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-opt-in=kotlinx.coroutines.FlowPreview")
+    }
 }
 
 application {
     mainClass.set("com.spectralogic.bp.bench.AppKt")
+}
+
+tasks.wrapper {
+    // to upgrade the gradle wrapper, bump the version below and run ./gradlew wrapper twice
+    gradleVersion = "8.1.1"
 }
